dca5def678c7ea8e09e60ad7e422b15ab13ad571
SainTurDaY27 <poom52344@gmail.com>
2021-10-11 11:56:11 +0700

    Completed to refactor pizzashop.

--- pizza.py
+++ pizza.py
@@ -1,24 +1,47 @@
+from enum import Enum
+
+
+class PizzaSize(Enum):
+    # Enum members written as: name = value
+    small = {'base_price': 120, 'topping': 20}
+    medium = {'base_price': 200, 'topping': 25}
+    large = {'base_price': 300, 'topping': 30}
+    jumbo = {'base_price': 320, 'topping': 35}
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def price(self):
+        return self.value['base_price']
+
+    @property
+    def topping_price(self):
+        return self.value['topping']
+
+
 class Pizza:
     """A pizza with a size and optional toppings."""
 
-    def __init__(self, size):
+    def __init__(self, size: PizzaSize):
+        if not isinstance(size, PizzaSize):
+            raise TypeError("size must be a PizzaSize")
         self.size = size
         self.toppings = []
 
-    def getPrice(self):
+    def get_price(self):
         """Price of pizza depends on size and number of toppings."""
-        if self.size == 'small':
-            price = 120 + 20*len(self.toppings)
-        elif self.size == 'medium':
-            price = 200 + 20*len(self.toppings)
-        elif self.size == 'large':
-            price = 280 + 20*len(self.toppings)
-        else:
-            raise ValueError("Unknown pizza size "+self.size)
-        return price
-    
-    def addTopping(self, topping):
+        return self.size.price + self.size.topping_price*len(self.toppings)
+
+    def add_topping(self, topping):
         """Add a topping to the pizza"""
         if topping not in self.toppings:
             self.toppings.append(topping)
 
+    def __str__(self):
+        description = str(self.size)
+        if self.toppings:
+            description += " pizza with " + ", ".join(self.toppings)
+        else:
+            description += " plain cheeze pizza"
+        return description
